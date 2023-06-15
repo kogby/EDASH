6cf49b735ea02f98f9d733054b2050e645f60ef8
unknown <kogby0507@gmail.com>
2023-06-15 11:10:14 +0800

    semester final

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # EDASH
-dashboard and helper for eda
+
+- ### em_example.ipynb :
+
+The example showcase of applying EM-Algorithm for imputation and information metrics calculation.
+
+- ### em_runtime_visual.ipynb :
+
+The code for experimenting with the running time and R^2 of EM and KNN method.
+
+- ### continuous.py:
+
+The implementation of information metric.
+
+- ### utils.py :
+
+The implemntation of EM-Algorithm imputation and Simulating missing values.
+
+- ### entropy.ipynb :
+
+Testing whether information metric works fine.
+
+- ### knn_simulate.ipynb :
+
+The implementation of KNN and testing. Packages are used.
+
+- ### Todo
+
+For the convenience of testing and implementation, we seperate the process of imputation and information metric.
+
+We'll rearrange the module of imputation and information metric.
--- utils.py
+++ utils.py
@@ -15,9 +15,11 @@ def impute_em(X, max_iter=3000, eps=1e-05):
     - Key 'C' stores the np.array that specifies the original missing entries
       of X.
     """
+
     cols = X.columns
     X = X.to_numpy()
     nr, nc = X.shape
+    # num of rows, num of columns
     C = np.isnan(X) == False
 
     # Collect M_i and O_i's
@@ -26,6 +28,7 @@ def impute_em(X, max_iter=3000, eps=1e-05):
     O = one_to_nc * C - 1
 
     # Generate Mu_0 and Sigma_0
+    # Obeserved mean only
     Mu = np.nanmean(X, axis=0)
     observed_rows = np.where(np.isnan(sum(X.T)) == False)[0]
     S = np.cov(X[observed_rows,].T)
@@ -41,19 +44,23 @@ def impute_em(X, max_iter=3000, eps=1e-05):
         for i in range(nr):
             S_tilde[i] = np.zeros(nc**2).reshape(nc, nc)
             if set(O[i,]) != set(one_to_nc - 1):  # missing component exists
-                M_i, O_i = M[i,][M[i,] != -1], O[i,][O[i,] != -1]
+                M_i, O_i = M[i,][M[i,] != -1], O[i,][O[i,] != -1]  # M_i, O_i
                 S_MM = S[np.ix_(M_i, M_i)]
                 S_MO = S[np.ix_(M_i, O_i)]
                 S_OM = S_MO.T
                 S_OO = S[np.ix_(O_i, O_i)]
+                # @ stands for matrix multiplication
                 Mu_tilde[i] = Mu[np.ix_(M_i)] + S_MO @ np.linalg.inv(S_OO) @ (
                     X_tilde[i, O_i] - Mu[np.ix_(O_i)]
                 )
+                # Imputation
                 X_tilde[i, M_i] = Mu_tilde[i]
                 S_MM_O = S_MM - S_MO @ np.linalg.inv(S_OO) @ S_OM
                 S_tilde[i][np.ix_(M_i, M_i)] = S_MM_O
+        # New mu and variances
         Mu_new = np.mean(X_tilde, axis=0)
         S_new = np.cov(X_tilde.T, bias=1) + reduce(np.add, S_tilde.values()) / nr
+        # Check convergence
         no_conv = (
             np.linalg.norm(Mu - Mu_new) >= eps
             or np.linalg.norm(S - S_new, ord=2) >= eps
